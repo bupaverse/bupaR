2f6789a256e769c6e593c33b269aab7dd9cbab5c
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2016-11-30 16:13:25 +0100

    Renamed to bupaR

---
+++ R/bupar.R
@@ -0,0 +1,10 @@
+#' @title bupaR - Business Process Analysis in R
+#' @docType package
+#' @name bupaR
+#' @description bupaR is a collection of functions and other packages which can be used for (business) process analysis in R.
+#'
+#' @importFrom magrittr %>%
+#'
+#'
+NULL
+
