d44552e7889270504c83e6d8d013ad5b4a1d8ae5
JANSSENSWILLEN Gert <gert.janssenswillen@uhasselt.be>
2022-01-11 17:50:38 +0100

    Activity log group by

--- R/group_by_activity_instance.R
+++ R/group_by_activity_instance.R
@@ -4,17 +4,17 @@
 #' @description Group an event log by activity instance identifier
 #' @inheritParams group_by_case
 #' @export group_by_activity_instance
-group_by_activity_instance <- function(.log) {
+group_by_activity_instance <- function(log) {
 	UseMethod("group_by_activity_instance")
 }
 
 
 #' @export
-group_by_activity_instance.eventlog <- function(.log) {
-	group_by(.log, !!activity_instance_id_(.log))
+group_by_activity_instance.eventlog <- function(log) {
+	group_by(log, !!activity_instance_id_(log))
 }
 
 #' @export
-group_by_activity_instance.activitylog <- function(.log) {
-	group_by(.log, !!activity_instance_id_(.log))
+group_by_activity_instance.activitylog <- function(log) {
+	group_by(log, !!activity_instance_id_(log))
 }
