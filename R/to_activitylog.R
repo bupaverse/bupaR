2bb7254336f7c56780e3948cf05686d5e34b293a
JANSSENSWILLEN Gert <gert.janssenswillen@uhasselt.be>
2022-01-13 14:49:29 +0100

    to_activitylog git status

---
+++ R/to_activitylog.R
@@ -0,0 +1,51 @@
+
+#' Title
+#'
+#' @param eventlog
+#'
+#' @return
+#' @export
+#'
+
+to_activitylog <- function(eventlog)
+	UseMethod("to_activitylog")
+
+#' @export
+
+to_activitylog.eventlog <- function(eventlog) {
+
+	.order <- NULL
+
+	if(!is.null(suppressMessages(detect_resource_inconsistencies(eventlog)))) {
+		stop("Eventlog contains resource inconsistencies. First use fix_resource_inconsistencies to fix problem")
+	}
+	mapping <- mapping(eventlog)
+	eventlog <- standardize_lifecycle(eventlog)
+
+	eventlog %>%
+		as.data.frame() %>%
+		group_by(.data[[activity_instance_id(eventlog)]]) %>%
+		mutate(.order = min(.order)) %>%
+		spread(.data[[lifecycle_id(eventlog)]], .data[[timestamp(eventlog)]]) -> activitylog
+
+
+	### Check if the columns start and complete exist. If not, initiate them to NA
+	if(!("start" %in% colnames(activitylog))){
+		warning("No start events were found. Creating and initialising 'start' to NA.")
+		activitylog$start <- lubridate::NA_POSIXct_
+	}
+	if(!("complete" %in% colnames(activitylog))){
+		warning("No complete events were found. Creating and initialising 'complete' to NA.")
+		activitylog$complete <- lubridate::NA_POSIXct_
+	}
+
+	activitylog(as.data.frame(activitylog),
+				case_id = case_id(mapping),
+				activity_id = activity_id(mapping),
+				resource_id = resource_id(mapping),
+				lifecycle_ids = as.vector(c("start","complete", str_subset(lifecycle_labels(eventlog), c("(start)|(complete)"), negate = T))),
+				order = ".order"
+	)
+
+
+}
