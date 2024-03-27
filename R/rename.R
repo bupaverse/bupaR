ef2e57f2dbd75e599a91d28022708df1e9fd29f7
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2024-03-27 16:52:54 +0100

    Fix rename. Partial fix #63

--- R/rename.R
+++ R/rename.R
@@ -2,9 +2,9 @@
 
 
 #' @title Rename log
-#' @name count
 #' @param .data \code{\link{log}}: Object of class \code{\link{eventlog}} or \code{\link{activitylog}}.
 #' @param ... Variables to rename. Use "new_name" = "old_name" to rename selected variables.
+#' @name rename
 #' @importFrom dplyr rename
 #' @export
 dplyr::rename
@@ -19,11 +19,34 @@ rename.log <- function(.data, ...) {
   renames <- list(...)
   mapping <- mapping(.data)
 
-  for(i in 1:6) {
-    if(mapping[[i]] %in% renames)
-      mapping[[i]] <- names(renames)[which(renames == mapping[[i]])]
+  if(mapping$case_identifier %in% renames) {
+    mapping$case_identifier <- names(renames)[renames == mapping$case_identifier]
+
+  }
+  if(mapping$activity_identifier          %in% renames) {
+    mapping$activity_identifier          <- names(renames)[renames == mapping$activity_identifier         ]
+  }
+  if(mapping$resource_identifier          %in% renames) {
+    mapping$resource_identifier          <- names(renames)[renames == mapping$resource_identifier         ]
   }
 
+  if(is.eventlog(.data)) {
+    if(mapping$activity_instance_identifier %in% renames) {
+      mapping$activity_instance_identifier <- names(renames)[renames == mapping$activity_instance_identifier]
+    }
+    if(mapping$lifecycle_identifier         %in% renames) {
+      mapping$lifecycle_identifier         <- names(renames)[renames == mapping$lifecycle_identifier        ]
+    }
+    if(mapping$timestamp_identifier         %in% renames) {
+      mapping$timestamp_identifier         <- names(renames)[renames == mapping$timestamp_identifier        ]
+    }
+  } else {
+    for(i in 1:length(mapping$timestamps)) {
+      if(mapping$timestamps[i] %in% renames) {
+        mapping$timestamps[i] <- names(renames)[renames == mapping$timestamps[i]]
+      }
+    }
+  }
   .data %>%
     as_tibble() %>%
     rename(...) %>%
